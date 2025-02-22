f9a75500e12b9c4589c46f5d3cb48ff747fa0062
agayushh <agayush088@gmail.com>
2025-02-22 19:26:06 +0530

    added zods

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { searchAtom } from "../states/atoms";
 
 export default function Searchbar() {
   const [search, setSearch] = useRecoilState(searchAtom);
 
-  //   useEffect(()=>{
-  //     localStorage.setItem("Prevsearched", search);
-  //   },[search])
+  // useEffect(()=>{
+  //   localStorage.setItem("Prevsearched", search);
+  // },[search])
+  // unlimited renders
 
   return (
     <input
@@ -18,7 +19,6 @@ export default function Searchbar() {
       value={search}
       className="mr-5 p-1 rounded-md border-2"
       onChange={(e) => {
-        console.log(e.target.value)
         setSearch(e.target.value);
         // localStorage.setItem("Prevsearched", search);
         localStorage.setItem("Prevsearched", e.target.value);
--- src/data/items.js
+++ src/data/items.js
@@ -1,13 +1,38 @@
 import axios from "axios";
+import { z } from "zod";
 
 const API_BASE_URL = "https://fakestoreapi.com";
 
 export const fetchProducts = async () => {
   try {
     const response = await axios.get(`${API_BASE_URL}/products`);
-    return response.data;
+    const data = response.data;
+
+    const validatedData = dataSchema.safeParse(data);
+
+    if (validatedData.success === false) {
+      console.log(validatedData.error.issues);
+      return [];
+    }
+
+    return validatedData.data;
   } catch (error) {
     console.log("unable to fetch data due to", error);
     return [];
   }
 };
+
+const dataSchema = z.array(
+  z.object({
+    id: z.number(),
+    title: z.string(),
+    price: z.number(),
+    description: z.string(),
+    category: z.string(),
+    image: z.string().url(),
+    rating: z.object({
+      rate: z.number(),
+      count: z.number(),
+    }),
+  })
+);
