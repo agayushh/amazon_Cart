f399e8302b962ab826c8b2e91b410d1a872ee1ab
agayushh <agayush088@gmail.com>
2025-02-24 19:04:46 +0530

    added routes to navbar icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,23 @@
-import React, { useState } 
-from "react";
+import React, { useState } from "react";
 import { CiShoppingCart } from "react-icons/ci";
 import { useRecoilValue } from "recoil";
 import { cartItem } from "../states/atoms";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const ItemsCount = useRecoilValue(cartItem);
-  
+
   return (
     <div>
       <div className="bg-blue-600 p-4 text-white flex justify-between">
-        <p>Amazon.in</p>
+        <Link to={"/"}>Amazon.in</Link>
         <div className="flex gap-x-3">
           <p>Hello User, </p>
           {""}
-          <CiShoppingCart size={22} />
+
+          <Link to="/cart">
+            <CiShoppingCart size={22} />
+          </Link>
 
           <div className="rounded-full h-4 w-4 bg-orange-50 absolute ml-[100px] mt-[-6px] text-black text-center text-sm">
             {ItemsCount}
