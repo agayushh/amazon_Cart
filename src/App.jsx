176a43c960df9f5227d40c435b2d5c5de9b544da
agayushh <agayush088@gmail.com>
2025-02-24 19:03:20 +0530

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AddtoCart from "./components/AddtoCart";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="h-[200vh]">
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/cart" element={<AddtoCart />}></Route>
+      </Routes>
+    
     </div>
+    </BrowserRouter>
   );
 }
 
