19daee5b72d6c03912979845cb1002a82252a0d7
agayushh <agayush088@gmail.com>
2025-02-25 09:46:29 +0530

    finalizing navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { CiShoppingCart } from "react-icons/ci";
 import { useRecoilValue } from "recoil";
-import { cartItem } from "../states/atoms";
+import { items } from "../states/atoms";
 import { Link } from "react-router-dom";
 
 export default function Navbar() {
-  const ItemsCount = useRecoilValue(cartItem);
+  const _items = useRecoilValue(items);
+
+  const cartItems = _items.filter((item) => item.cartQuantity > 0);
+  const cartCount = cartItems.length;
 
   return (
     <div>
@@ -20,7 +23,7 @@ export default function Navbar() {
           </Link>
 
           <div className="rounded-full h-4 w-4 bg-orange-50 absolute ml-[100px] mt-[-6px] text-black text-center text-sm">
-            {ItemsCount}
+            {cartCount}
           </div>
         </div>
       </div>
