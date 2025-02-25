89aee90b85735e677d93a9d74d1080e60d736a5c
agayushh <agayush088@gmail.com>
2025-02-25 09:44:50 +0530

    card items functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { items } from "../states/atoms";
 
 export default function Card({ product, isPresent }) {
-  const [enableAddtoCart, setEnableAddtoCart] = useState(false);
-  const [quantity, setQuantity] = useState(0);
+  const setItems = useSetRecoilState(items);
+
+  const enableAddtoCart = product.cartQuantity > 0;
 
   return (
     <li
       key={product.id}
-      className={`border-2 p-4 flex flex-col group ${isPresent ? "" : "hidden"} `}
-
+      className={`border-2 p-4 flex flex-col group ${
+        isPresent ? "" : "hidden"
+      } `}
     >
-      <img src={product.image} alt={product.title} className="h-80 w-80 group-hover:scale-105  duration-200" />
+      <img
+        src={product.image}
+        alt={product.title}
+        className="h-80 w-80 group-hover:scale-105  duration-200"
+      />
       <div className="flex-grow" />
       <p className="text-wrap w-fit h-[80px] flex items-end">
         {product.title} - ${product.price}
       </p>
       <button
         className="mt-3 bg-yellow-500 font-bold p-2"
-
         onClick={() => {
-          setEnableAddtoCart((prevState) => !prevState);
-          if (quantity === 0 && !enableAddtoCart) {
-            setQuantity(1);
+          if (product.cartQuantity === 0) {
+            setItems((prev) =>
+              prev.map((item) =>
+                item.id === product.id
+                  ? {
+                      ...item,
+                      cartQuantity: 1,
+                    }
+                  : item
+              )
+            );
           }
         }}
       >
@@ -35,14 +50,36 @@ export default function Card({ product, isPresent }) {
       >
         <button
           className="border-2 px-2"
-          onClick={() => setQuantity((c) => c - 1)}
+          onClick={() =>
+            setItems((prev) =>
+              prev.map((item) =>
+                item.id === product.id
+                  ? {
+                      ...item,
+                      cartQuantity: item.cartQuantity - 1,
+                    }
+                  : item
+              )
+            )
+          }
         >
           -
         </button>
-        <p className="border-2 px-2">{quantity}</p>
+        <p className="border-2 px-2">{product.cartQuantity}</p>
         <button
           className="border-2  px-2"
-          onClick={() => setQuantity((c) => c + 1)}
+          onClick={() =>
+            setItems((prev) =>
+              prev.map((item) =>
+                item.id === product.id
+                  ? {
+                      ...item,
+                      cartQuantity: item.cartQuantity + 1,
+                    }
+                  : item
+              )
+            )
+          }
         >
           +
         </button>
